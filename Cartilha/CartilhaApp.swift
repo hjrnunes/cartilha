71cd60e96dabd980df26a0b8330a0d526697593d
Henrique Nunes <hjrnunes@gmail.com>
2023-07-05 22:16:19 +0200

    Initial Commit

---
+++ Cartilha/CartilhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartilhaApp.swift
+//  Cartilha
+//
+//  Created by Henrique Nunes on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CartilhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
